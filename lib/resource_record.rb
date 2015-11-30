8498dc3f01221fd575940cc51a8996f9f736427c
Sam Russell <sam.russell@powershop.co.nz>
2015-11-30 14:44:34 +1300

    resource record done, due for refactor

--- lib/resource_record.rb
+++ lib/resource_record.rb
@@ -1,2 +1,25 @@
+require 'name'
+
 class ResourceRecord
+  attr_reader :name, :rdata
+
+  def initialize(name: name, record_type: record_type, record_class: record_class, record_ttl: record_ttl, rdata: rdata)
+    @name = name
+    @record_type = record_type
+    @record_class = record_class
+    @record_ttl = record_ttl
+    @rdata = rdata
+  end
+
+  def self.unpack(packed_stream, packed_response)
+    name = Name.unpack(packed_stream, packed_response)
+    record_type = packed_stream.read(2).unpack('n').first
+    record_class = packed_stream.read(2).unpack('n').first
+    record_ttl = packed_stream.read(4).unpack('N').first
+    rdata_length = packed_stream.read(2).unpack('n').first
+
+    rdata = packed_stream.read(rdata_length)
+
+    new(name: name, record_type: record_type, record_class: record_class, record_ttl: record_ttl, rdata: rdata)
+  end
 end
